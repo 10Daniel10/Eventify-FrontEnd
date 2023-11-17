6b52475ae13d7600e0ba68be2919969c3ece487f
martinagattas <martigattas@hotmail.com>
2023-11-17 20:40:04 -0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,29 @@
-module.exports = {
-  reactStrictMode: true,
+// next.config.js
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'style-loader',
-          options: {
-            // Establece la prioridad de los estilos
-            insert: 'body',
-          },
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            // Establece la prioridad de los estilos
-            importLoaders: 1,
-          },
-        },
-      ],
-    });
+    // Encuentra la regla de estilo de Material-UI
+    const muiStyleRule = config.module.rules.find(
+      (rule) =>
+        rule.oneOf && rule.oneOf.some((subRule) => subRule.issuer && subRule.issuer.include && subRule.issuer.include.includes('@mui')),
+    );
+
+    // Establece la prioridad de los estilos de Material-UI
+    if (muiStyleRule) {
+      muiStyleRule.issuer = { ...muiStyleRule.issuer, priority: 1 };
+    }
+
+    // Encuentra la regla de tus estilos CSS modules
+    const cssModulesRule = config.module.rules.find(
+      (rule) => rule.oneOf && rule.oneOf.some((subRule) => String(subRule.test) === String(/\.module\.css$/)),
+    );
+
+    // Establece la prioridad de tus estilos CSS modules
+    if (cssModulesRule) {
+      cssModulesRule.issuer = { ...cssModulesRule.issuer, priority: 2 };
+    }
+
     return config;
   },
-}
+});

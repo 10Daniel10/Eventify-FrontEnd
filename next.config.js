4a43f204c9ed5355d64740070c789d5a204828f0
martinagattas <martigattas@hotmail.com>
2023-11-17 19:23:08 -0300

    next config > css order

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    return config
-  }
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader',
+          options: {
+            // Establece la prioridad de los estilos
+            insert: 'body',
+          },
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            // Establece la prioridad de los estilos
+            importLoaders: 1,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }

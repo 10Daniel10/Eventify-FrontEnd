bdcb308edcfad8a1cab7b991a774851a616ae4b8
martinagattas <martigattas@hotmail.com>
2023-11-25 15:59:45 -0300

    fix(globals): change font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,300;0,6..12,400;0,6..12,600;0,6..12,900;1,6..12,300;1,6..12,400;1,6..12,600;1,6..12,900&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />

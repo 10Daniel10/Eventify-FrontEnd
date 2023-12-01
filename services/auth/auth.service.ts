9bf12881c755eea7014368bbad58daa570f20765
martinagattas <martigattas@hotmail.com>
2023-12-01 19:59:38 -0300

    cta service

--- services/auth/auth.service.ts
+++ services/auth/auth.service.ts
@@ -1,20 +1,31 @@
+import { TUserData } from 'types';
 import { LoginFormT, ValidateEmailT } from 'types/auth/LoginForm.types';
 import { RegisterFormT } from 'types/auth/RegisterForm.types';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export const userEmail = async (data: ValidateEmailT): Promise<any> => {
-  const userData = JSON.stringify(data);
-  const response = await fetch(`${apiUrl}/userEmail`, {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    method: "GET",
-    body: userData,
-  });
+export const validateUserEmail = async (userEmail: string): Promise<boolean> => {
+  try {
+    const response = await fetch(`${apiUrl}/users`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
 
-  return await response.json();
+    if (!response.ok) {
+      throw new Error(`Error al obtener los datos del servidor. Código ${response.status}`);
+    }
+
+    const userDataArray: TUserData[] = await response.json();
+    const emailExists = userDataArray.some(user => user.email === userEmail);
+
+    return emailExists;
+  } catch (error) {
+    console.error("El correo que ingresaste no es válido:", error);
+    return false;
+  }
 }
 
 export const loginUser = async (data: LoginFormT): Promise<any> => {
---
+++ types/TUsers.ts
@@ -0,0 +1,16 @@
+export type TUserData = {
+  id: number;
+  email: string;
+  firstname: string;
+  lastname: string;
+  type: string;
+}
+
+export type TUserLogin = {
+  email: string;
+  password: string;
+}
+
+export type TUserEmail = {
+  email: string;
+}

5ad6c8ca15e9b0281eef4066c5a9903ce734cf26
bxplayer <bxplayer10@gmail.com>
2023-12-04 23:29:14 -0300

    se agrego un modal al terminar al reservar

--- components/services/ServicesReservation.tsx
+++ components/services/ServicesReservation.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -9,6 +9,7 @@ import { IServices } from 'interfaces';
 import { addProduct } from 'eventapp/services/cart/cart.services';
 import { useRouter } from 'next/router';
 import { getIdUser } from 'eventapp/services/users/users.service';
+import Swal from 'sweetalert2'
 
 type TInitialData = {
   dateReservation: Date;
@@ -19,26 +20,64 @@ const initialData: TInitialData = {
 
 export const ServiceReservation: FC<IServices> = ({service}) => {
   const router = useRouter();
-  const userId = getIdUser();  
+  const userId = getIdUser(); 
+  const [disabled, setDisabled] = useState(false) 
   const { control, handleSubmit, formState: {errors} } = useForm<TInitialData>();
-  const { id, name, bookedDates } = service;  
-console.log(bookedDates)
+  const { id, name, bookedDates } = service;   
   
-  const numeroRandom = Math.floor(Math.random() * 9) + 1;
   const onSubmit: SubmitHandler<TInitialData> = async (data) => {  
     var fechaComoCadena = `${data.dateReservation}`;
     const reserved = bookedDates?.some(x => x == `${data.dateReservation}`);
-    if(reserved) alert("reservado")
+    if(reserved){ 
+      showSwalError();
+      return
+    }
     addProduct(userId,fechaComoCadena, service);
+    showSwalSuccess(fechaComoCadena);
   };
-
 
   const sendToCart = () => {
     router.push('/cart');
   }
 
+  const showSwalError = () => {
+    Swal.fire({
+      icon: "error",
+      title: "Lo siento",
+      text: "Esta fecha ya se encuentra reservada",
+      confirmButtonText: "Ver otros servicios",      
+    }).then((result) => {
+      if (result.isConfirmed) {
+        router.push('/services');
+      }
+    });
+  }
+
+  const showSwalSuccess = (fechaComoCadena : string) => {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: `Reservada realizada!`,
+      html: `Se reservo <b>${name}</b> el día : <b>${fechaComoCadena}</b>`,   
+      showCancelButton: true,
+      confirmButtonColor: "#2ABCC8",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Seguir viendo servicios",
+      cancelButtonText: "Finalizar Reservas",
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        router.push('/services');
+      }
+      if(result.isDismissed){
+        sendToCart()
+      }
+    });
+    setDisabled(true)
+  }
+
   return(
-    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+    <Box component="form" onSubmit={handleSubmit(onSubmit)}>      
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <CustomInput

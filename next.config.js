4042e652a3d68a490410b23138e1a4d3c09bdf5c
martinagattas <martigattas@hotmail.com>
2023-12-06 20:43:25 -0300

    config bucket

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 // next.config.js
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/eventify-bucket/**',
+      },
+    ],
+  },
 };

b139a66e2a388d6ca0cc77c5f617ec69d24e6754
martinagattas <martigattas@hotmail.com>
2023-12-07 20:29:08 -0300

    fix(pdf)

--- services/providers/providers.service.ts
+++ services/providers/providers.service.ts
@@ -24,14 +24,6 @@ export const getProviderServices = async (id: number):Promise<any> => {
   return await response.json();
 }
 
-export const getReports = async (providerId: string, dateFrom: string, dateTo: string):Promise<any> => {
-  const response = await fetch(`${apiUrl}/report/${providerId}?startDate=${dateFrom}T00:00:00&endDate=${dateTo}T23:59:59`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    }
-  })
-
-  return await response;
+export const getReports = (providerId: string, dateFrom: string, dateTo: string):string => {
+  return `${apiUrl}/report/${providerId}?startDate=${dateFrom}T00:00:00&endDate=${dateTo}T23:59:59`;
 }
